b3965540a4c58b5b2e5c3d62706360efbeea4d62
Eric Zhao <140433074+mykinglebron@users.noreply.github.com>
2024-04-16 22:34:51 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -6,12 +6,24 @@ public class Digits
 	private ArrayList<Integer> digitList;
 
 	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    
+	{  
+	  digitList= new ArrayList<Integers>();
+		if(num==0){
+		digitList.add(0);}
+	int n = num;
+		while(n>0){
+			digitList.add(0,n%10);
+			n=n/10;
+		}
 	}
 
 	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
+	{ 
+		for (int i=0; i<digitList.size()-1;i++){
+			if(digitList.get(i)>=digitList.get(i+1))
+				return false;
+			return true;
+		}
 		
 
 	}
