becb1727c9f21aebccdb6cbb91be542f012a4548
Eric Zhao <140433074+mykinglebron@users.noreply.github.com>
2024-04-22 19:30:55 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -3,33 +3,32 @@ import java.util.ArrayList;
 public class Digits
 {
 
-	private ArrayList<Integer> digitList;
+private ArrayList<Integer> digitList;
 
-	public Digits(int num)
-	{  
-	  digitList= new ArrayList<Integer>();
-		if(num==0)
-		digitList.add(0);
-	int n = num;
-		while(n>0){
-			digitList.add(0,n%10);
-			n=n/10;
-		}
-	}
+public Digits(int num)
+{ /* to be implemented in part (a) */
+digitList = new ArrayList <Integer>();
+if(num == 0){
+digitList.add(0);
+}
+while(num > 0){
+digitList.add(0, num%10);
+num = num/10;
+}
+}
 
-	public boolean isStrictlyIncreasing()
-	{ 
-		for (int i=0; i<digitList.size()-1;i++){
-			if(digitList.get(i+1)<=digitList.get(i))
-				return false;
-			return true;
-		}
-		
+public boolean isStrictlyIncreasing()
+{ /* to be implemented in part (b) */
+for(int i = 0; i < digitList.size() - 1; i++){
+if(digitList.get(i) >= digitList.get(i + 1)){
+return false;
+}
+}
+return true;
+}
 
-	}
-	
-	public String toString()
-	{
-		return digitList.toString();
-	}
+public String toString()
+{
+return digitList.toString();
+}
 }
